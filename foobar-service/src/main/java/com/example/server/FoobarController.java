ada5ce55fa87cfb0c303eae3d912021d543e29a5
Gerhard Preisinger <gerhard@radiantracoon.at>
2025-08-29 15:49:12 +0200

    writes a log to test baggage attachment there

--- foobar-service/src/main/java/com/example/server/FoobarController.java
+++ foobar-service/src/main/java/com/example/server/FoobarController.java
@@ -1,5 +1,7 @@
 package com.example.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 class FoobarController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FoobarController.class);
 
   @GetMapping
   public ResponseEntity<String> sayFoobar(){
+    LOGGER.info("Received a foobar call!");
     return ResponseEntity.ok("foobar");
   }
 
