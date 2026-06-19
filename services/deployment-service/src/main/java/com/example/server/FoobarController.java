2ee1e60051398abbcceaf17e0e925061a5a95375
Gerhard Preisinger <gerhard@radiantracoon.at>
2026-06-19 09:40:33 +0200

    adds multi service setup

---
+++ services/deployment-service/src/main/java/com/example/server/FoobarController.java
@@ -0,0 +1,34 @@
+package com.example.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+class FoobarController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FoobarController.class);
+
+  private final FooService fooService;
+
+  public FoobarController(FooService fooService) {
+    this.fooService = fooService;
+  }
+
+  @GetMapping
+  public ResponseEntity<String> sayFoobar(){
+    LOGGER.info("Received a foobar call!");
+
+    try {
+      fooService.explode();
+    } catch (Exception e) {
+      /* suppress error in this span */
+    }
+
+    return ResponseEntity.ok("foobar");
+  }
+
+}
